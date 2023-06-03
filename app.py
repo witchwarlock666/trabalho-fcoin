5609dad5a9acf0dd054a234e7142564e6d68f0eb
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2023-06-02 21:18:32 -0300

    debug false

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def page_not_found(error):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
